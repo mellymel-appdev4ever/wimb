7881677e70eb94894f22fd88391516e173a02a2f
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-08-09 17:29:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
 import streamlit as st
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import col
 
-# Initialize connection.
-conn = st.experimental_connection("snowpark", type="snowpark")
-
-st.stop()
-
-# Load the table as a dataframe using the Snowpark Session.
-@st.cache_data
-def load_table():
-    with conn.safe_session() as session:
-        return session.table('mytable').to_pandas()
-
-df = load_table()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.NAME} has a :{row.PET}:")
+st.title('My Page')
